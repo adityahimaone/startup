835f1a36779962e7b27195bb856f1530059f37bf
Adit <adityahimaone@gmail.com>
2021-09-02 22:51:22 +0700

    Add Login API Hash

--- handler/user.go
+++ handler/user.go
@@ -44,3 +44,36 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	response := helper.APIResponse("Account has been resgistered", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
+
+//user memasukan input
+//input ditangkap handler
+//mapping dari input user ke input struct
+//input struct parsing ke service
+//service mencari dg bantuan repository user dengan email x
+//jika ketemu mencocokkan password
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokenx")
+	response := helper.APIResponse("Login Success", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -29,13 +29,38 @@ func main() {
 
 	//akan dialihkan ke register user
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 	router.Run()
-	//fill input
-	/*	userInput := user.RegisterUserInput{}
-		userInput.Name = "Test"
-		userInput.Email = "aa@mail.com"
-		userInput.Occupation = "soft dev"
-		userInput.Password = "secret"
-		userService.RegisterUser(userInput)*/
-
 }
+
+
+//komentar
+
+//fill input
+/*	userInput := user.RegisterUserInput{}
+	userInput.Name = "Test"
+	userInput.Email = "aa@mail.com"
+	userInput.Occupation = "soft dev"
+	userInput.Password = "secret"
+	userService.RegisterUser(userInput)*/
+
+/*input := user.LoginInput{
+	Email:    "aa@mail.com",
+	Password: "secret",
+}
+user, err := userService.Login(input)
+if err != nil {
+	fmt.Println(err.Error())
+	fmt.Println("Salah")
+}
+fmt.Println(user.Email)
+fmt.Println(user.Name)*/
+/*userByEmail, err := userRepository.FindByEmail("adiyt@mail.com")
+if err != nil {
+	fmt.Println(err.Error())
+}
+if userByEmail.ID == 0 {
+	fmt.Println("user not found")
+} else {
+	fmt.Println(userByEmail.Name)
+}*/
